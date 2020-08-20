9535c2222cd8f24c0758eac8955feaea124507c9
iamdarkes <josephdarkes@gmail.com>
2020-08-19 23:13:33 -0700

    display published date on page

--- src/templates/page.js
+++ src/templates/page.js
@@ -12,9 +12,10 @@ export default class Page extends React.Component {
                     <section id="main" className="wrapper">
                         <div className="inner">
                             <h1 className="major">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
-                            {this.props.pageContext.frontmatter.content_img_path && 
+                            {this.props.pageContext.frontmatter.content_img_path &&
                                 <span className="image fit"><img src={safePrefix(this.props.pageContext.frontmatter.content_img_path)} alt="" /></span>
                             }
+                            <h5>{"Published "}{_.get(this.props, 'pageContext.frontmatter.date')}</h5>
                             {htmlToReact(_.get(this.props, 'pageContext.html'))}
                         </div>
                     </section>
